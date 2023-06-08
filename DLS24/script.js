4edc21fa5fec2668dc7d0a92d254f5504895f697
haideptrai9 <131457284+haideptrai9@users.noreply.github.com>
2023-06-08 02:35:58 -0700

    Update script.js

--- DLS24/script.js
+++ DLS24/script.js
@@ -102,19 +102,17 @@ function drawCard() {
 
         // First Name
         ctx.textAlign = 'center';
-        ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
-        ctx.font = '16px Renogare';
+        ctx.fillStyle = '#00000080';
+        ctx.font = boldText + '16px Renogare';
         ctx.fillText(value('firstName'), 130, 281);
-        if (checked('legendary') && checked('maxed'))  
-        ctx.fillStyle = '#FFFFFF';
+        if (checked('legendary') && checked('maxed')) ctx.fillStyle = '#FFFFFF';
 
 
         // Last Name
         ctx.fillStyle = '#ffffff';
-        ctx.font = '25px Renogare';
+        ctx.font = boldText + '25px Renogare';
         ctx.fillText(value('lastName'), 130, value('firstName') === '' ? 299 : 305);
-        if (checked('legendary') && checked('maxed')) 
-        ctx.fillStyle = '#FFFFFF';
+        if (checked('legendary') && checked('maxed')) ctx.fillStyle = '#FFFFFF';
 
         // Stats
         ctx.font = '20px Renogare';
